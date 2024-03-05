30bbf0b878e16a137aee0c84457188122b2c718d
Santiago Castro Herrera <159493032+Luismi0415@users.noreply.github.com>
2024-03-04 19:17:02 -0500

    momento 4

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -4,4 +4,6 @@ class Asignatura:
         self._nombre = nombre
         self._salon = salon
 
-    
+    def __str__(self):
+        return f"{self._nombre} {self._salon}"
+        
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -16,7 +16,8 @@ class Grupo:
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}"
     
     @classmethod
     def asignarNombre(cls, nombre="Grado 6"):
